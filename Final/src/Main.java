fab0a528990abf9b777602ff7f1a0efef2cd27d5
yyjni <yyjni@LAPTOP-N160KKK4>
2021-06-12 17:39:41 +0900

    ryj main

--- Final/src/Main.java
+++ Final/src/Main.java
@@ -29,7 +29,7 @@ public class Main extends JPanel implements ActionListener{
 		titlepanel = new Panel();
 		Color color = new Color(239, 239, 143);
         setBackground(color);
-		titlelabel = new JLabel("윤용익 교수님과 함께하는 \"객체 타자연습\"");
+		titlelabel = new JLabel("<html><br/>윤용익 교수님과 함께하는 \"객체 타자연습\"<br/><br/></html>");
 		
 		titlelabel.setFont(font);
 		titlepanel.add(titlelabel);
